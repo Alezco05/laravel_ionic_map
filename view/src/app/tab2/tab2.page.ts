1ac8776f7be1622e3131902b9278dddb0bc7d2c0
Alezco05 <jhon.mario.05@hotmail.com>
2020-02-13 16:56:40 -0500

    Vista

--- view/src/app/tab2/tab2.page.ts
+++ view/src/app/tab2/tab2.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
+import * as Mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
   constructor() {}
-
+  title = 'view';
+  mapa: Mapboxgl.Map;
+  ngOnInit(){
+    (Mapboxgl as any).accessToken = environment.mapboxKey;
+    this.mapa = new Mapboxgl.Map({
+    container: 'mapa', // container id
+    style: 'mapbox://styles/mapbox/outdoors-v11',
+    center: [-74.810913,10.985246], // LNG, LAT
+    zoom: 14 // starting zoom
+    });
+     
+    // Add zoom and rotation controls to the map.
+    this.mapa.addControl(new Mapboxgl.NavigationControl());
+    //this.crearMarcador(-74.810913,10.985246);
+  }
+  crearMarcador(lng:number, lat:number){
+    const marker = new Mapboxgl.Marker({
+      draggable: true
+      })
+      .setLngLat([lng, lat])
+      .addTo(this.mapa);
+      marker.on('dragend', () => console.log(marker.getLngLat()));
+  }
 }
