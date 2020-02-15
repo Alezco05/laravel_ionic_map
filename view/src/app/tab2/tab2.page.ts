1e1afc71d74a291606e543cf5fe1f2fca8b13767
Alezco05 <jhon.mario.05@hotmail.com>
2020-02-15 13:59:59 -0500

    Map Working

--- view/src/app/tab2/tab2.page.ts
+++ view/src/app/tab2/tab2.page.ts
@@ -6,25 +6,26 @@ import * as Mapboxgl from 'mapbox-gl';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements OnInit{
+export class Tab2Page{
 
   constructor() {}
   title = 'view';
   mapa: Mapboxgl.Map;
-  ngOnInit(){
+  ionViewDidEnter() { this.createMap(); }
+  createMap(){
     (Mapboxgl as any).accessToken = environment.mapboxKey;
     this.mapa = new Mapboxgl.Map({
     container: 'mapa', // container id
     style: 'mapbox://styles/mapbox/outdoors-v11',
-    center: [-74.810913,10.985246], // LNG, LAT
+    center: [-74.810913, 10.985246], // LNG, LAT
     zoom: 14 // starting zoom
     });
      
     // Add zoom and rotation controls to the map.
     this.mapa.addControl(new Mapboxgl.NavigationControl());
-    //this.crearMarcador(-74.810913,10.985246);
+    this.crearMarcador(-74.810913,10.985246);
   }
-  crearMarcador(lng:number, lat:number){
+   crearMarcador(lng: number, lat: number){
     const marker = new Mapboxgl.Marker({
       draggable: true
       })
