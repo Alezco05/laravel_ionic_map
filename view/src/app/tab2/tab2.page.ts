b78fcc8a645cc5de5e9dd958ec2a3eb18c90ed1c
Alezco05 <jhon.mario.05@hotmail.com>
2020-02-15 20:09:52 -0500

    Multiples Marcadores en el mapa

--- view/src/app/tab2/tab2.page.ts
+++ view/src/app/tab2/tab2.page.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment.prod';
 import * as Mapboxgl from 'mapbox-gl';
+import { PqrService } from '../services/pqr.service';
+import { Pqr } from '../interfaces/pqr';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  constructor() {}
+  pqrs: Pqr[] = [];
+  constructor(private pqrService: PqrService) {
+  }
   title = 'view';
   mapa: Mapboxgl.Map;
   ionViewDidEnter() {
-    this.createMap();
+    this.pqrService.getPqrs().subscribe(
+      resp => {
+        this.pqrs = resp;
+        this.createMap(this.pqrs);
+      }
+    );
+    console.log(this.pqrs);
   }
-  createMap() {
+  createMap(pqrs: Pqr[]) {
     (Mapboxgl as any).accessToken = environment.mapboxKey;
     this.mapa = new Mapboxgl.Map({
       container: 'mapa', // container id
@@ -25,17 +35,20 @@ export class Tab2Page {
     this.mapa.addControl(new Mapboxgl.NavigationControl());
     const geolocate = new   Mapboxgl.GeolocateControl();
     this.mapa.addControl(geolocate);
-    geolocate.on('geolocate', (e)  => {
+    geolocate.on('geolocate', (e,pqrs)  => {
       const lon = e.coords.longitude;
       const lat = e.coords.latitude;
       const position = [lon, lat];
       this.crearMarcador(lon, lat);
-      console.log(position);
     });
+    if(pqrs.length > 0){
+      pqrs.forEach(pqr => this.crearMarcador(pqr.long, pqr.lat));
+    }
   }
   crearMarcador(lng: number, lat: number) {
     const marker = new Mapboxgl.Marker({
-      draggable: true
+      draggable: true,
+      color: 'red'
     })
       .setLngLat([lng, lat])
       .addTo(this.mapa);
