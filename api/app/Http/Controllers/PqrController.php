e77a1a6f89ca464c703664dad1870556ee4787cf
Alezco05 <jhon.mario.05@hotmail.com>
2020-02-16 17:56:41 -0500

    Arreglando problemas

--- api/app/Http/Controllers/PqrController.php
+++ api/app/Http/Controllers/PqrController.php
@@ -30,6 +30,7 @@ class PqrController extends Controller
             'in_type' => 'required|max:255',
             'in_code' => 'required|numeric',
             'problem_id'=> 'required|numeric',
+            'neighbor_id'=> 'required|numeric',
             'address' => 'required|max:255',
             'issue' => 'required',
             'phone'=> 'required|numeric',
