71054b4cdbffd1dee10f850a89dc39873d0e3748
Alezco05 <jhon.mario.05@hotmail.com>
2020-02-17 10:43:41 -0500

    Opciones de filtrado en el mapa agregadas

--- api/app/Http/Controllers/NeighborsController.php
+++ api/app/Http/Controllers/NeighborsController.php
@@ -84,10 +84,12 @@ class NeighborsController extends Controller
     }
 
     public function getNames(){
-        $neighbors = DB::table('pqrs')
-        ->select('neighbors.name', 'pqrs.neighbor_id')
-        ->join('neighbors','pqrs.neighbor_id','=','neighbors.id')
-        ->groupBy('neighbors.name','pqrs.neighbor_id')
+        $neighbors = DB::TABLE('pqrs')
+        ->SELECT('neighbors.name AS neighbor', 'problem_lists.problem AS problem', 'infrastructures.name AS infrastructure', 'pqrs.neighbor_id', 'pqrs.problem_id', 'pqrs.infrastructure_id')
+        ->JOIN('neighbors','pqrs.neighbor_id','=','neighbors.id')
+        ->JOIN('problem_lists','pqrs.problem_id','=','problem_lists.id')       
+        ->JOIN('infrastructures','pqrs.infrastructure_id','=','infrastructures.id')
+        ->groupBy('neighbors.name','problem_lists.problem','infrastructures.name','pqrs.neighbor_id','pqrs.infrastructure_id','pqrs.problem_id')
         ->get();
         return $neighbors;
     }
